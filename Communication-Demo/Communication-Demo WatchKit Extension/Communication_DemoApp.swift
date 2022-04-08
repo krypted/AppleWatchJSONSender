53d90d7284f2ae6af6c0ed047d63ed57b8e6ad2e
Charles Edge <krypted@me.com>
2022-04-08 10:47:13 -0500

    Add files via upload

---
+++ Communication-Demo/Communication-Demo WatchKit Extension/Communication_DemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  Communication_DemoApp.swift
+//  Communication-Demo WatchKit Extension
+//
+//  Created by Charles Edge on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Communication_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ Communication-Demo/Communication-Demo/Communication_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Communication_DemoApp.swift
+//  Communication-Demo
+//
+//  Created by Charles Edge on 07/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Communication_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
